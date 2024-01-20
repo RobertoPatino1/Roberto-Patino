f18c7a1ea588eb89a0546db86f38c2a4609aa2a1
Roberto Patiño <rapatino@espol.edu.ec>
2024-01-20 01:59:55 -0500

    Added footer and animations

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
