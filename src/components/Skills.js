87564f9f0c1c2781dd5758ea0aa259d5572eb848
Roberto Patiño <rapatino@espol.edu.ec>
2024-01-19 21:37:59 -0500

    Added skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,71 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+//Image import
+import meter1 from "../assets/logo/meter1.svg";
+import meter2 from "../assets/logo/meter2.svg";
+import meter3 from "../assets/logo/meter3.svg";
+import colorSharp from "../assets/logo/color-sharp.png";
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  //Todo: Add more skills
+  return (
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua
+                <br></br>Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                classNmae="skill-slider"
+              >
+                <div className="item">
+                  <img src={meter1} alt="Skill-Image1"></img>
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="Skill-Image2"></img>
+                  <h5>Cyber-Security</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Skill-Image3"></img>
+                  <h5>Operative Systems</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img src={colorSharp} className="background-image-left" alt=""></img>
+    </section>
+  );
+};
