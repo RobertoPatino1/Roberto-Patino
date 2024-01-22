891a5d3b41d967f8e8517848a27db0dc8f373875
Roberto Patiño <rapatino@espol.edu.ec>
2024-01-22 02:12:18 -0500

    Added project file

---
+++ src/assets/js/projects.js
@@ -0,0 +1,168 @@
+import projImg1 from "../img/project-img1.png";
+import projImg2 from "../img/project-img2.png";
+import projImg3 from "../img/project-img3.png";
+
+export const dev_projects = [
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+];
+
+export const low_level_projects = [
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+];
+
+export const ds_projects = [
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+];
+
+export const game_projects = [
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+];
+
+export const desktop_projects = [
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg1,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg2,
+  },
+  {
+    title: "Business Startup",
+    description: "Design & Development",
+    imgUrl: projImg3,
+  },
+];
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,44 +1,49 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+// import projImg1 from "../assets/img/project-img1.png";
+// import projImg2 from "../assets/img/project-img2.png";
+// import projImg3 from "../assets/img/project-img3.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
+import { dev_projects } from "../assets/js/projects";
+import { low_level_projects } from "../assets/js/projects";
+import { ds_projects } from "../assets/js/projects";
+import { game_projects } from "../assets/js/projects";
+import { desktop_projects } from "../assets/js/projects";
 
 export const Projects = () => {
-  const projects = [
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-  ];
+  // const projects = [
+  //   {
+  //     title: "Business Startup",
+  //     description: "Design & Development",
+  //     imgUrl: projImg1,
+  //   },
+  //   {
+  //     title: "Business Startup",
+  //     description: "Design & Development",
+  //     imgUrl: projImg2,
+  //   },
+  //   {
+  //     title: "Business Startup",
+  //     description: "Design & Development",
+  //     imgUrl: projImg3,
+  //   },
+  //   {
+  //     title: "Business Startup",
+  //     description: "Design & Development",
+  //     imgUrl: projImg1,
+  //   },
+  //   {
+  //     title: "Business Startup",
+  //     description: "Design & Development",
+  //     imgUrl: projImg2,
+  //   },
+  //   {
+  //     title: "Business Startup",
+  //     description: "Design & Development",
+  //     imgUrl: projImg3,
+  //   },
+  // ];
 
   return (
     <section className="project" id="projects">
@@ -96,7 +101,7 @@ export const Projects = () => {
                     >
                       <Tab.Pane eventKey="first">
                         <Row>
-                          {projects.map((project, index) => (
+                          {dev_projects.map((project, index) => (
                             <ProjectCard key={index} {...project} />
                           ))}
                         </Row>
