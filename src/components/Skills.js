285ac993066c37e4b80c28fe6c773f51479be843
Roberto Patiño <rapatino@espol.edu.ec>
2024-01-21 23:38:37 -0500

    Added all images

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,7 @@ import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import colorSharp from "../assets/img/color-sharp.png";
+// import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
@@ -65,7 +65,7 @@ export const Skills = () => {
           </Col>
         </Row>
       </Container>
-      <img src={colorSharp} className="background-image-left" alt=""></img>
+      {/* <img src={colorSharp} className="background-image-left" alt=""></img> */}
     </section>
   );
 };
